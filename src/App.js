21eb2105d5e64a7634e828d30c8b48e9756c9f8c
udaykumardl <udaykumardl744@gmail.com>
2024-05-31 15:07:05 +0530

    adding a modal via react portal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
 import { Fragment } from 'react';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header/>
       <main>
         <Meals />
